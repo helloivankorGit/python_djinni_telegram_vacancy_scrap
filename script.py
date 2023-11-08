f3a20baa9401ee57df0348df58066e94d5c4f095
helloivankorGit <helloivankor@gmail.com>
2023-11-08 19:02:06 +0200

    rework

--- script.py
+++ script.py
@@ -1,26 +1,38 @@
-from telethon.sync import TelegramClient, events
 import asyncio
+import logging
+from telethon.sync import TelegramClient, events
 
-# Replace these values with your own API credentials
-api_id = ''
-api_hash = ''
+# Configuration (consider moving to a config file or environment variables)
+API_ID = ''
+API_HASH = ''
+SESSION_NAME = 'djinni'
+BOT_URL = 'https://t.me/djinni_jobs_bot'
 
-# Define your session name (can be any string)
-session_name = 'djinni'
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
-async def check_new_messages():
-    async with TelegramClient(session_name, api_id, api_hash) as client:
-        # Use the bot username or chat ID to target the specific bot/chat
-        bot_entity = await client.get_input_entity('https://t.me/djinni_jobs_bot')
+async def get_bot_entity(client):
+    try:
+        return await client.get_input_entity(BOT_URL)
+    except Exception as e:
+        logging.error(f"Error getting bot entity: {e}")
+        return None
 
-        @client.on(events.NewMessage(chats=bot_entity))
-        async def handle_new_message(event):
-            # Handle new messages here
-            message_text = event.message.text
-            print(f"New message: {message_text}")
+async def listen_for_messages(client, bot_entity):
+    @client.on(events.NewMessage(chats=bot_entity))
+    async def handle_new_message(event):
+        message_text = event.message.text
+        logging.info(f"New message: {message_text}")
 
-        print("Listening for new messages...")
-        await client.run_until_disconnected()
+async def main():
+    async with TelegramClient(SESSION_NAME, API_ID, API_HASH) as client:
+        bot_entity = await get_bot_entity(client)
+        if bot_entity:
+            await listen_for_messages(client, bot_entity)
+            logging.info("Listening for new messages...")
+            await client.run_until_disconnected()
+        else:
+            logging.error("Bot entity not found. Exiting...")
 
 if __name__ == '__main__':
-    asyncio.run(check_new_messages())
+    asyncio.run(main())
